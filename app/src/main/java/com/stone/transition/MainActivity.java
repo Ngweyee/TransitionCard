18fd45b5857b95784b831e0f6bc1d485b6c5c83b
DESKTOP-I187MP9\MIT <ngweyeeshoon.1@gmail.com>
2018-03-23 11:44:53 +0630

    changelog: -first commit

---
+++ app/src/main/java/com/stone/transition/MainActivity.java
@@ -0,0 +1,105 @@
+package com.stone.transition;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private ViewPager viewPager;
+    private List<CommonFragment> fragments = new ArrayList<>();
+    private final String[] imageArray = {"assets://image1.jpg", "assets://image2.jpg", "assets://image3.jpg", "assets://image4.jpg", "assets://image5.jpg"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
+                getWindow()
+                        .getDecorView()
+                        .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            } else {
+                getWindow()
+                        .setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            }
+        }
+
+        initImageLoader();
+        fillViewPager();
+    }
+
+    private void fillViewPager() {
+        viewPager = findViewById(R.id.viewpager);
+
+        // 1. viewPager parallax，PageTransformer
+        viewPager.setPageTransformer(false, new CustPagerTransformer(this));
+
+        for (int i = 0; i < 10; i++) {
+            // 10 fragment
+            fragments.add(new CommonFragment());
+        }
+
+        viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                CommonFragment fragment = fragments.get(position % 10);
+                fragment.bindData(imageArray[position % imageArray.length]);
+                return fragment;
+            }
+
+            @Override
+            public int getCount() {
+                return 666;
+            }
+        });
+
+
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private void initImageLoader() {
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                this)
+                .memoryCacheExtraOptions(480, 800)
+                // default = device screen dimensions
+                .threadPoolSize(3)
+                .threadPriority(Thread.NORM_PRIORITY - 1)
+                .tasksProcessingOrder(QueueProcessingType.FIFO)
+                .denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024).memoryCacheSizePercentage(13) // default
+                .discCacheSize(50 * 1024 * 1024) //
+                .discCacheFileCount(100) //
+                .discCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
+                .imageDownloader(new BaseImageDownloader(this)) // default
+                .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
+                .writeDebugLogs().build();
+
+        // 2.ImageLoader
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(config);
+    }
+
+}
